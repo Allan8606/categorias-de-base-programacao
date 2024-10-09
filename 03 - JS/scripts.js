0c26e30e6be099e030f9841065f90d117fef038c
Allan8606 <allan-musico@hotmail.com>
2024-10-09 19:02:15 -0300

    arquivos da aula de JS

--- 03 - JS/scripts.js
+++ 03 - JS/scripts.js
@@ -1,3 +1,19 @@
-let name = 'Jose Arthur Lima Costa'
 
-console.log(typeof name);
+//Descobrir se o numero é par ou impar
+// const num1 = 13
+// if (num1 % 2 === 0) {
+//   console.log('É par');
+// }else{
+//   console.log('É impar');
+// }
+// num1 % 2 === 0 ? console.log('É par') : console.log('É impar');
+
+const anoAtual = 2024
+
+// if (anoAtual % 4 === 0 && anoAtual % 100 !== 0 || (anoAtual % 400 === 0)) {
+//   console.log('É bissexto');
+// }else{
+//   console.log('Não é bissexto');
+// }
+anoAtual % 4 === 0 && anoAtual % 100 !== 0 || (anoAtual % 400 === 0) ? console.log('É bissexto') : console.log('Não é bissexto');
+
